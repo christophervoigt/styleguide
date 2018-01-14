39b438c2bc1f139420db9685c3d03ed1440fe1d2
Chlorophyllkid <chlorophyllkid@gmail.com>
2018-01-14 11:02:30 +0100

    Upgrade assets to 0.0.4

--- dist/modules/atoms/atom.js
+++
@@ -1,1 +0,0 @@
-var atom=function(){"use strict";window.addEventListener("load",function(){new atom});function t(t){const o={selector:"body",scope:document};this.options=Object.assign({},o,t)}return t.prototype.subscribe=function(t,o){const n=this,{selector:e}=n.options;document.querySelectorAll(e).forEach(e=>{e.addEventListener(t,t=>{t.stopPropagation(),o.notify(n,t.type)},!0)})},t}();
