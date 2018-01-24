71ed5e0f219d7d1b76f9fd82d38b8f7e504664e1
Chlorophyllkid <chlorophyllkid@gmail.com>
2018-01-24 12:56:52 +0100

    Add copy task for static files
    resolve #3

---
+++ tasks/static.build.js
@@ -0,0 +1,43 @@
+/* eslint no-console: ["off", { allow: ["warn"] }] */
+
+const path = require('path');
+const chalk = require('chalk');
+const Cattleman = require('cattleman');
+const shell = require('shelljs');
+
+const srcPath = 'src';
+const distPath = process.env.NODE_ENV === 'production' ? 'dist' : 'app';
+
+async function build(module) {
+  const srcPathDirs = srcPath.split('/');
+
+  const file = path.parse(module);
+  const moduleDirs = file.dir.split(path.sep);
+  const targetDirs = moduleDirs.splice(srcPathDirs.length, moduleDirs.length);
+  const targetPath = path.normalize(targetDirs.join(path.sep));
+  const targetDir = path.join(distPath, targetPath);
+
+  await shell.mkdir('-p', targetDir);
+  await shell.cp(module, targetDir);
+}
+
+async function rebuild(module) {
+  console.log('STATIC: move', chalk.green(module));
+  build(module);
+}
+
+(async () => {
+  const cattleman = new Cattleman(srcPath);
+  const modules = cattleman.gatherFiles([
+    '.woff',
+    '.woff2',
+    '.ttf',
+    '.json',
+  ]);
+
+  await Promise.all(modules.map(async (module) => {
+    await build(module);
+  }));
+})();
+
+exports.rebuild = rebuild;
--- tasks/watch.js
+++ tasks/watch.js
@@ -7,6 +7,7 @@ const rebuildCSS = require('./css.build').rebuild;
 const rebuildHTML = require('./html.build').rebuild;
 const rebuildIMG = require('./image.build').rebuild;
 const rebuildJS = require('./javascript.build').rebuild;
+const rebuildSTATIC = require('./static.build').rebuild;
 
 const srcPath = 'src';
 
@@ -47,5 +48,13 @@ watch(srcPath, {
   filter: /\.js$/,
 }, async (event, name) => {
   await rebuildJS(name);
+  browserSync.reload();
+});
+
+watch(srcPath, {
+  recursive: true,
+  filter: /\.woff$|\.woff2$|\.ttf$|\.json$/,
+}, async (event, name) => {
+  await rebuildSTATIC(name);
   browserSync.reload();
 });
