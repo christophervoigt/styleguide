21e8b324ead45ecb81ccd64a3d02129468ec864a
Chlorophyllkid <chlorophyllkid@gmail.com>
2018-05-09 10:01:33 +0200

    add Sass breakpoints

--- src/styleguide/styleguide.js
+++ src/styleguide/styleguide.js
@@ -1,8 +1,10 @@
 
-import highlight from './sg-highlight/highlight';
+import base from '../base/base';
 import sidebar from './sg-sidebar/sidebar';
+import highlight from './sg-highlight/highlight';
 
 export default function styleguide() {
-  highlight();
+  base();
   sidebar();
+  highlight();
 }
