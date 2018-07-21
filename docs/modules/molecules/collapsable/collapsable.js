a99b24be676ad9e56341c24f579638b015c3519c
Christopher Voigt <chlorophyllkid@gmail.com>
2018-07-21 17:54:54 +0200

    update docs

---
+++ docs/modules/molecules/collapsable/collapsable.js
@@ -0,0 +1,37 @@
+var collapsable = (function () {
+  'use strict';
+
+  document.addEventListener('DOMContentLoaded',function(){collapsable()});
+
+  function collapsable() {
+    var selector = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '[data-collapse]';
+
+    var elements = document.querySelectorAll(selector);
+
+    if (elements.length) {
+      Array.prototype.forEach.call(elements, function (element) {
+        element.addEventListener('click', function () {
+          var parent = element.parentNode;
+          var targetSelector = element.dataset.collapse;
+          var target = parent.querySelector(targetSelector);
+
+          if (!target.classList.contains('expanded')) {
+            target.style.cssText = 'height: auto';
+            var height = target.scrollHeight;
+            target.style.cssText = 'height: 0';
+            target.style.cssText = 'max-height: ' + height + 'px;';
+          } else {
+            target.style.cssText = '';
+          }
+
+          element.classList.toggle('expanded');
+          target.classList.toggle('expanded');
+        });
+      });
+    }
+  }
+
+  return collapsable;
+
+}());
+//# sourceMappingURL=collapsable.js.map
